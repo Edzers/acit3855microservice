53b991cf3282e4507a989badc6db0ca1cc2bcb5f
Edzers <eddie0906@hotmail.com>
2023-11-02 10:02:21 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -61,49 +61,54 @@ def get_card_input_event(index):
     
 
 # Test the function
-event, status_code = get_card_input_event(3)
-print(event)
+# event, status_code = get_card_input_event(3)
+# print(event)
 
 def get_rate_seller_event(index):
-    """Get rate_seller event in History."""
     hostname = f"{app_config['events']['hostname']}:{app_config['events']['port']}"
     client = KafkaClient(hosts=hostname)
     topic = client.topics[str.encode(app_config["events"]["topic"])]
 
-    # Here we reset the offset on start so that we retrieve
-    # messages at the beginning of the message queue.
-    # To prevent the for loop from blocking, we set the timeout to
-    # 1000ms. There is a risk that this loop never stops if the
-    # index is large and messages are constantly being received!
     consumer = topic.get_simple_consumer(
         reset_offset_on_start=True,
         consumer_timeout_ms=1000
     )
     logger.info("Retrieving rate_seller at index %d", index)
     
-    try:
-        current_index = 1
-        for msg in consumer:
-            msg_str = msg.value.decode('utf-8')
-            msg = json.loads(msg_str)
-            if msg.get('event_type') == 'rate_seller' and current_index == index:
-                # Found the event at the index you want
-                # return the event and a 200 status code
+    current_index = 0
+    for msg in consumer:
+        msg_str = msg.value.decode('utf-8')
+        msg = json.loads(msg_str)
+        if msg.get('type') == 'rate_seller':
+            if current_index == index:
                 return msg, 200
-            if msg.get('event_type') == 'rate_seller':
-                # Increment the index for rate_seller events only
-                current_index += 1
-    except Exception as e:
-        logger.error("No more messages found or an error occurred: %s", str(e))
-        logger.error("Could not find rate_seller at index %d", index)
-        return {"message": "Not Found"}, 404
+            current_index += 1
 
+    logger.error("Could not find rate_seller at index %d", index)
+    return {"message": "Not Found"}, 404
 
 # Test the function
-event, status_code = get_rate_seller_event(1)
-print(event)
+# try:
+#     event, status_code = get_rate_seller_event(1)
+#     print(event, status_code)
+# except Exception as e:
+#     print("An error occurred:", str(e))
+    
 
-# app.add_api('openapi.yml', arguments={'title': 'Audit API'})
+@app.route('/card_input', methods=['GET'])
+def get_card_input_event_route():
+    index = int(request.args.get('index'))
+    event, status_code = get_card_input_event(index)
+    return jsonify(event), status_code
 
-# if __name__ == "__main__":
-#     app.run(port=8200)  
+@app.route('/rate_seller', methods=['GET'])
+def get_rate_seller_event_route():
+    index = int(request.args.get('index'))
+    event, status_code = get_rate_seller_event(index)
+    return jsonify(event), status_code
+
+
+app.add_api('openapi.yml', arguments={'title': 'Audit API'})
+
+if __name__ == "__main__":
+    app.run(port=8110)  
