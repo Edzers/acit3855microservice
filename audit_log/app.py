f673f7355b1a3551100e808f68552b6290467ec0
Edzers <eddie0906@hotmail.com>
2023-11-06 21:57:59 -0800

    Edits

--- audit_log/app.py
+++ audit_log/app.py
@@ -35,7 +35,7 @@ def get_card_input_event(index):
 
     consumer = topic.get_simple_consumer(
         reset_offset_on_start=True,
-        consumer_timeout_ms=1000
+        consumer_timeout_ms=2000
     )
     logger.info("Retrieving card_input at index %d", index)
     
@@ -50,40 +50,6 @@ def get_card_input_event(index):
 
     logger.error("Could not find card_input at index %d", index)
     return {"message": "Not Found"}, 404
-# def get_card_input_event(index):
-#     """Get card_input event in History."""
-#     hostname = f"{app_config['events']['hostname']}:{app_config['events']['port']}"
-#     client = KafkaClient(hosts=hostname)
-#     topic = client.topics[str.encode(app_config["events"]["topic"])]
-
-#     # Here we reset the offset on start so that we retrieve
-#     # messages at the beginning of the message queue.
-#     # To prevent the for loop from blocking, we set the timeout to
-#     # 1000ms. There is a risk that this loop never stops if the
-#     # index is large and messages are constantly being received!
-#     consumer = topic.get_simple_consumer(
-#         reset_offset_on_start=True,
-#         consumer_timeout_ms=1000
-#     )
-#     logger.info("Retrieving card_input at index %d", index)
-    
-#     try:
-#         current_index = 0
-#         for msg in consumer:
-#             msg_str = msg.value.decode('utf-8')
-#             msg = json.loads(msg_str)
-#             if msg.get('type') == 'card_input' and current_index == index:
-#                 # Found the event at the index you want
-#                 # return the event and a 200 status code
-#                 return msg, 200
-#             if msg.get('type') == 'card_input':
-#                 # Increment the index for card_input events only
-#                 current_index += 1
-#     except Exception as e:
-#         logger.error("No more messages found or an error occurred: %s", str(e))
-#         logger.error("Could not find card_input at index %d", index)
-#         return {"message": "Not Found"}, 404  # Ensure two values are returned
-    
 
 # Test the function
 # event, status_code = get_card_input_event(3)
@@ -129,8 +95,13 @@ def get_card_input_event_route():
 @app.route('/rate_seller', methods=['GET'])
 def get_rate_seller_event_route():
     index = int(request.args.get('index'))
-    event, status_code = get_rate_seller_event(index)
-    return jsonify(event), status_code
+    try:
+        event, status_code = get_rate_seller_event(index)
+        return jsonify(event), status_code
+    except Exception as e:
+        logger.error("Unhandled exception: %s", str(e), exc_info=True)
+        return jsonify({"message": "Internal Server Error"}), 500
+
 
 
 app.add_api('openapi.yml', arguments={'title': 'Audit API'})
