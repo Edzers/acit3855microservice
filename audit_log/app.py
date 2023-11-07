3ccb4f105dd0479cccc3ec04242bd0eb2d2e1969
Edzers <eddie0906@hotmail.com>
2023-11-06 20:28:17 -0800

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -29,38 +29,60 @@ logger = logging.getLogger('basicLogger')
 
 
 def get_card_input_event(index):
-    """Get card_input event in History."""
     hostname = f"{app_config['events']['hostname']}:{app_config['events']['port']}"
     client = KafkaClient(hosts=hostname)
     topic = client.topics[str.encode(app_config["events"]["topic"])]
 
-    # Here we reset the offset on start so that we retrieve
-    # messages at the beginning of the message queue.
-    # To prevent the for loop from blocking, we set the timeout to
-    # 1000ms. There is a risk that this loop never stops if the
-    # index is large and messages are constantly being received!
     consumer = topic.get_simple_consumer(
         reset_offset_on_start=True,
         consumer_timeout_ms=1000
     )
     logger.info("Retrieving card_input at index %d", index)
     
-    try:
-        current_index = 0
-        for msg in consumer:
-            msg_str = msg.value.decode('utf-8')
-            msg = json.loads(msg_str)
-            if msg.get('type') == 'card_input' and current_index == index:
-                # Found the event at the index you want
-                # return the event and a 200 status code
+    current_index = 0
+    for msg in consumer:
+        msg_str = msg.value.decode('utf-8')
+        msg = json.loads(msg_str)
+        if msg.get('type') == 'card_input':
+            if current_index == index:
                 return msg, 200
-            if msg.get('type') == 'card_input':
-                # Increment the index for card_input events only
-                current_index += 1
-    except Exception as e:
-        logger.error("No more messages found or an error occurred: %s", str(e))
-        logger.error("Could not find card_input at index %d", index)
-        return {"message": "Not Found"}, 404  # Ensure two values are returned
+            current_index += 1
+
+    logger.error("Could not find card_input at index %d", index)
+    return {"message": "Not Found"}, 404
+# def get_card_input_event(index):
+#     """Get card_input event in History."""
+#     hostname = f"{app_config['events']['hostname']}:{app_config['events']['port']}"
+#     client = KafkaClient(hosts=hostname)
+#     topic = client.topics[str.encode(app_config["events"]["topic"])]
+
+#     # Here we reset the offset on start so that we retrieve
+#     # messages at the beginning of the message queue.
+#     # To prevent the for loop from blocking, we set the timeout to
+#     # 1000ms. There is a risk that this loop never stops if the
+#     # index is large and messages are constantly being received!
+#     consumer = topic.get_simple_consumer(
+#         reset_offset_on_start=True,
+#         consumer_timeout_ms=1000
+#     )
+#     logger.info("Retrieving card_input at index %d", index)
+    
+#     try:
+#         current_index = 0
+#         for msg in consumer:
+#             msg_str = msg.value.decode('utf-8')
+#             msg = json.loads(msg_str)
+#             if msg.get('type') == 'card_input' and current_index == index:
+#                 # Found the event at the index you want
+#                 # return the event and a 200 status code
+#                 return msg, 200
+#             if msg.get('type') == 'card_input':
+#                 # Increment the index for card_input events only
+#                 current_index += 1
+#     except Exception as e:
+#         logger.error("No more messages found or an error occurred: %s", str(e))
+#         logger.error("Could not find card_input at index %d", index)
+#         return {"message": "Not Found"}, 404  # Ensure two values are returned
     
 
 # Test the function
