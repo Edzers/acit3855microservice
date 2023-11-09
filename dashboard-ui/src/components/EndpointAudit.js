e2c4036d07667088d8fca096205cf09f029cd478
Edzers <eddie0906@hotmail.com>
2023-11-09 14:14:38 -0800

    Update EndpointAudit.js

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -6,19 +6,23 @@ export default function EndpointAudit(props) {
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
 	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+    const [index, setIndex] = useState(null); // Add a state for the index
 
     const getAudit = () => {
         fetch(`http://acit3855group45kafka.eastus2.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
-            .then((result)=>{
-				console.log("Received Audit Results for " + props.endpoint)
+            .then((result) => {
+                console.log("Received Audit Results for " + props.endpoint)
                 setLog(result);
+                setIndex(rand_val); // Set the index here to ensure it is in sync with the data
                 setIsLoaded(true);
-            },(error) =>{
-                setError(error)
+            }, (error) => {
+                setError(error);
                 setIsLoaded(true);
             })
     }
+    
+
 	useEffect(() => {
 		const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
 		return() => clearInterval(interval);
@@ -32,9 +36,10 @@ export default function EndpointAudit(props) {
         
         return (
             <div>
-                <h3>{props.endpoint}-{rand_val}</h3>
+                <h3>{props.endpoint}-{index}</h3> 
                 {JSON.stringify(log)}
             </div>
         )
     }
 }
+
