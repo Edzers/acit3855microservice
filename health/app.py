ed1fa9119b615839cac3802fef1c6656f7a299c0
Edzers <eddie0906@hotmail.com>
2023-12-07 10:03:00 -0800

    Stuff

--- health/app.py
+++ health/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
 import requests
-import datetime
+from datetime import datetime
 import connexion
 from connexion import NoContent
-import logging
+import logging.config
 import yaml
 import os
-import json 
+import json
 
-app = connexion.FlaskApp(__name__, specification_dir='')
-
-# Check environment and load configurations
+# Load configurations based on the environment
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     print("In Test Environment")
     app_conf_file = "/config/app_conf.yml"
@@ -26,61 +24,57 @@ with open(app_conf_file, 'r') as f:
 
 with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
+
 logging.config.dictConfig(log_config)
 logger = logging.getLogger('basicLogger')
-logger.info("App Conf File: %s" % app_conf_file)
-logger.info("Log Conf File: %s" % log_conf_file)
 
-app = Flask(__name__)
+# Initialize Flask app for Connexion
+app = connexion.FlaskApp(__name__, specification_dir='')
+app.add_api("cardapi.yaml")  
+flask_app = app.app
 
+# Initialize the health status dictionary
 health_status = {
     "receiver": "Down",
     "storage": "Down",
     "processing": "Down",
     "audit": "Down",
-    "last_update": ""
+    "last_update": datetime.utcnow().isoformat()
 }
 
-# Function to save the health status to a JSON file
-def save_health_status():
-    with open('health_status.json', 'w') as f:
-        json.dump(health_status, f)
-
-# Modified check_health function to call save_health_status
+# Function to check the health of other services and update the status
 def check_health():
-    services = app_config['services']
-    for service_name, service_url in services.items():
+    for service_name, service_url in app_config['services'].items():
         try:
             response = requests.get(service_url, timeout=app_config['scheduler']['timeout_sec'])
             health_status[service_name] = "Running" if response.status_code == 200 else "Down"
         except requests.exceptions.RequestException:
             health_status[service_name] = "Down"
     health_status["last_update"] = datetime.utcnow().isoformat()
-    save_health_status()  # Save status to JSON file
+    # Save the health status to a JSON file
+    with open(app_config['datastore']['filename'], 'w') as f:
+        json.dump(health_status, f)
+    logger.info(f"Updated health status: {health_status}")
 
-# Modified get_health endpoint to read from the JSON file
+# Endpoint to get the health status of services
 @app.route('/health', methods=['GET'])
 def get_health():
     try:
-        with open('health_status.json', 'r') as f:
-            saved_health_status = json.load(f)
-        return jsonify(saved_health_status)
+        with open(app_config['datastore']['filename'], 'r') as f:
+            current_health_status = json.load(f)
+        return jsonify(current_health_status)
     except FileNotFoundError:
         logger.error("Health status file not found.")
         return jsonify(health_status)  # Return the in-memory status if file not found
 
-# Start scheduler
+# Initialize the scheduler to run the health check periodically
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=check_health, trigger="interval", seconds=20)
+scheduler.add_job(func=check_health, trigger="interval", seconds=app_config['scheduler']['period_sec'])
 scheduler.start()
 
-
+# Main entry point for the application
 if __name__ == "__main__":
-    # Load the health status from the file on startup if available
-    try:
-        with open('health_status.json', 'r') as f:
-            health_status = json.load(f)
-    except FileNotFoundError:
-        logger.error("Health status file not found. Starting with default statuses.")
-    app.run(port=8120)
-
+    # Run the health check once before starting to populate the status immediately
+    check_health()
+    # Run the Flask app
+    flask_app.run(port=8120, use_reloader=False)
