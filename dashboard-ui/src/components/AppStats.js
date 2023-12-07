7ed83a6cb751b909f7ba6aa2c585005b48f694ce
Edzers <eddie0906@hotmail.com>
2023-12-07 09:23:36 -0800

    Changes to health logging and dashboard

--- dashboard-ui/src/components/AppStats.js
+++ dashboard-ui/src/components/AppStats.js
@@ -1,22 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 
 export default function AppStats() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [stats, setStats] = useState({});
+    const [status, setStatus] = useState({});
     const [error, setError] = useState(null);
 
-    const getStats = () => {
-        fetch(`http://acit3855group45kafka.eastus2.cloudapp.azure.com:8100/stats`)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error(`HTTP error status: ${res.status}`);
-                }
-                return res.json();
-            })
+    // Function to fetch the service statuses
+    const getServiceStatus = () => {
+        fetch('http://acit3855group45kafka.eastus2.cloudapp.azure.com:8120/health')
+            .then(res => res.json())
             .then(
                 (result) => {
-                    console.log("Received Stats", result);
+                    setStatus(result);
+                },
+                (error) => {
+                    setError(error);
+                }
+            );
+    };
+
+    // Function to fetch the stats
+    const getStats = () => {
+        fetch('http://acit3855group45kafka.eastus2.cloudapp.azure.com:8100/stats')
+            .then(res => res.json())
+            .then(
+                (result) => {
                     setStats(result);
                     setIsLoaded(true);
                 },
@@ -28,18 +38,38 @@ export default function AppStats() {
     };
 
     useEffect(() => {
-        const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
-        return () => clearInterval(interval);
+        getStats();
+        getServiceStatus();
+        const statsInterval = setInterval(getStats, 2000); // Update stats every 2 seconds
+        const statusInterval = setInterval(getServiceStatus, 20000); // Update service status every 20 seconds
+        return () => {
+            clearInterval(statsInterval);
+            clearInterval(statusInterval);
+        };
     }, []);
 
+    // Function to format the last updated time
+    const formatLastUpdate = () => {
+        if (!status.last_update) return '...';
+        const lastUpdateTime = new Date(status.last_update);
+        const now = new Date();
+        const secondsAgo = Math.round((now - lastUpdateTime) / 1000);
+        return `${secondsAgo} seconds ago`;
+    };
+
     if (error) {
-        return <div className={"error"}>Error found when fetching from API: {error.message}</div>;
+        return <div className={"error"}>Error: {error.message}</div>;
     } else if (!isLoaded) {
         return <div>Loading...</div>;
     } else {
         return (
             <div>
-                <h1>Latest Stats</h1>
+                <h1>Latest Stats and Service Status</h1>
+                <div>Receiver: {status.receiver}</div>
+                <div>Storage: {status.storage}</div>
+                <div>Processing: {status.processing}</div>
+                <div>Audit: {status.audit}</div>
+                <div>Last Update: {formatLastUpdate()}</div>
                 <table className={"StatsTable"}>
                     <tbody>
                         <tr>
@@ -54,7 +84,6 @@ export default function AppStats() {
                             <td>Max Card Price: {stats['max_card_price']}</td>
                             <td>Max Seller Rating: {stats['max_seller_rating']}</td>
                         </tr>
-                            <td>Last Updated: {stats['last_updated']}</td>
                     </tbody>
                 </table>
             </div>
--- health/app.py
+++ health/app.py
@@ -7,6 +7,7 @@ from connexion import NoContent
 import logging
 import yaml
 import os
+import json 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 
@@ -31,6 +32,7 @@ logger.info("App Conf File: %s" % app_conf_file)
 logger.info("Log Conf File: %s" % log_conf_file)
 
 app = Flask(__name__)
+
 health_status = {
     "receiver": "Down",
     "storage": "Down",
@@ -39,23 +41,46 @@ health_status = {
     "last_update": ""
 }
 
+# Function to save the health status to a JSON file
+def save_health_status():
+    with open('health_status.json', 'w') as f:
+        json.dump(health_status, f)
+
+# Modified check_health function to call save_health_status
 def check_health():
-    services = {"receiver": "http://localhost:8080/health", "storage": "http://localhost:8090/health","processing": "http://localhost:8100/health", "audit": "http://localhost:8110/health"}
+    services = app_config['services']
     for service_name, service_url in services.items():
         try:
-            response = requests.get(service_url, timeout=5)
+            response = requests.get(service_url, timeout=app_config['scheduler']['timeout_sec'])
             health_status[service_name] = "Running" if response.status_code == 200 else "Down"
         except requests.exceptions.RequestException:
             health_status[service_name] = "Down"
     health_status["last_update"] = datetime.utcnow().isoformat()
+    save_health_status()  # Save status to JSON file
 
+# Modified get_health endpoint to read from the JSON file
 @app.route('/health', methods=['GET'])
 def get_health():
-    return jsonify(health_status)
+    try:
+        with open('health_status.json', 'r') as f:
+            saved_health_status = json.load(f)
+        return jsonify(saved_health_status)
+    except FileNotFoundError:
+        logger.error("Health status file not found.")
+        return jsonify(health_status)  # Return the in-memory status if file not found
 
+# Start scheduler
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=check_health, trigger="interval", seconds=20)
 scheduler.start()
 
+
 if __name__ == "__main__":
+    # Load the health status from the file on startup if available
+    try:
+        with open('health_status.json', 'r') as f:
+            health_status = json.load(f)
+    except FileNotFoundError:
+        logger.error("Health status file not found. Starting with default statuses.")
     app.run(port=8120)
+
